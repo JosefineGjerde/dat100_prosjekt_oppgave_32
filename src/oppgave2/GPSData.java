74535544e2de71ef2375dbf687350e0c0b9a940e
JosefineGjerde <111700277+JosefineGjerde@users.noreply.github.com>
2022-10-17 13:50:58 +0200

    Update GPSData.java

--- src/oppgave2/GPSData.java
+++ src/oppgave2/GPSData.java
@@ -1,68 +1,66 @@
-package oppgave2;
+package no.hvl.dat100ptc.oppgave2;
 
-import todo.TGPSData.*;
-import oppgave1.GPSPoint;
-import todo.TODO;
-
-import static oppgave2.GPSDataConverter.convert;
+import no.hvl.dat100ptc.TODO;
+import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class GPSData {
 
-    private GPSPoint[] gpspoints;
-    protected int antall = 0;
+	private GPSPoint[] gpspoints;
+	protected int antall = 0;
 
-    public GPSData(int n) {
+	public GPSData(int n) {
 
-    	
-        gpspoints = new GPSPoint[n];
+		// TODO - START
+		
+		gpspoints = new GPSPoint[n];
 
-    }
+		// TODO - SLUTT
+	}
 
-    public GPSPoint[] getGPSPoints() {
-        return this.gpspoints;
+	public GPSPoint[] getGPSPoints() {
+		return this.gpspoints;
+	}
+	
+	protected boolean insertGPS(GPSPoint gpspoint) {
 
-    }
+		boolean inserted = false;
 
-    protected boolean insertGPS(GPSPoint gpspoint) {
+		// TODO - START
+		try {
+			gpspoints[antall] = gpspoint;
+			antall++;
+			inserted = true;
+		}catch (Exception e) {
+		inserted = false;
+		
+		}
+		// TODO - SLUTT
+		return inserted;
+	}
 
-    	
-        boolean inserted = false;
+	public boolean insert(String time, String latitude, String longitude, String elevation) {
 
-        try {
+		GPSPoint gpspoint;
+		// TODO - SLUTT
+		return true;
+		
+	}
 
-                gpspoints[antall] = gpspoint;
-                antall ++;
-                inserted = true;
+	public void print() {
 
-        }catch (Exception e){
-            inserted = false;
+		System.out.println("====== Konvertert GPS Data - START ======");
 
-        }
-        return inserted;
+		// TODO - START
+		
+		for(int i = 0; i < gpspoints.length; i++) {
+	
+		System.out.printf("%s",gpspoints[i]);
+		}
+		// TODO - SLUTT
+		
+		System.out.println("====== Konvertert GPS Data - SLUTT ======");
 
-    }
-
-    public boolean insert(String time, String latitude, String longitude, String elevation) {
-
-        GPSPoint gpspoint = convert(time,latitude,longitude,elevation);
-        antall++;
-    	
-        return true;
-
-
-    }
-
-    public void print() {
-
-    	
-        System.out.println("====== Konvertert GPS Data - START ======");
-
-        for(int i = 0; i < gpspoints.length; i++){
-            System.out.printf("%s",gpspoints[i]);
-        }
-      
-        // System.out.println("====== Konvertert GPS Data - SLUTT ======");
-
-    }
+	}
 }
+
 
